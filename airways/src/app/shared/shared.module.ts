7111743bbc89b95844ad4612e09ba7b685e6f8e5
azozulya <bbbom@mail.ru>
2023-04-21 14:58:18 +0300

    feat: create shared module

--- airways/src/app/shared/shared.module.ts
+++ airways/src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import { MatButtonModule } from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    //MatButtonModule
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class SharedModule { }
