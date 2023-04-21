a130ba65d8e346ba92d6842e57e5f206a8881486
azozulya <bbbom@mail.ru>
2023-04-21 14:55:52 +0300

    feat: create route, register feature module

--- airways/src/app/app-routing.module.ts
+++ airways/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectFlightComponent } from './airways/pages/select-flight/select-flight.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'select-flight', component: SelectFlightComponent },
+  { path: '**', component: SelectFlightComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AirwaysModule } from './airways/airways.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AirwaysModule
   ],
   providers: [],
   bootstrap: [AppComponent]
