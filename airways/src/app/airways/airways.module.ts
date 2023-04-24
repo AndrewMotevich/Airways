ad97a5d142aeb4bf071058418091ac0fe72c3bf2
azozulya <bbbom@mail.ru>
2023-04-24 22:00:22 +0300

    feat: add pipes and directive to app module

--- airways/src/app/airways/airways.module.ts
+++ airways/src/app/airways/airways.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FlightDetailsComponent } from './components/step1/flight-details/flight-details.component';
 import { SelectFlightComponent } from './pages/select-flight/select-flight.component';
 import { SharedModule } from '../shared/shared.module';
+import { DurationPipe } from './pipe/duration.pipe';
+import { ColoredDirective } from './directives/colored.directive';
+import { FormatPricePipe } from './pipe/format-price.pipe';
 
 @NgModule({
-  declarations: [SelectFlightComponent, FlightDetailsComponent],
+  declarations: [SelectFlightComponent, FlightDetailsComponent, DurationPipe, ColoredDirective, FormatPricePipe],
   imports: [
     CommonModule,
     SharedModule,
