246b860084e60869283298998d8de4e2716ecd6f
azozulya <bbbom@mail.ru>
2023-04-24 22:09:28 +0300

    feat: add service to get flights

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AirwaysModule } from './airways/airways.module';
@@ -11,6 +12,7 @@ import { AirwaysModule } from './airways/airways.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     AirwaysModule
   ],
