6e14ee80a4f9287ef45d4e802cd34cc06c5b9901
azozulya <bbbom@mail.ru>
2023-04-29 18:44:53 +0300

    feat: add booking route

--- airways/src/app/app-routing.module.ts
+++ airways/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { SelectFlightComponent } from './booking/pages/select-flight/select-flight.component';
 import { MainPageComponent } from './booking/pages/main-page/main-page.component';
 import { Page404Component } from './booking/pages/page404/page404.component';
+import { BookingFlightComponent } from './booking/pages/booking-flight/booking-flight.component';
 
 const routes: Routes = [
-  { path: 'select-flight', component: SelectFlightComponent },
+  { path: 'select', component: SelectFlightComponent },
+  { path: 'booking', component: BookingFlightComponent },
   { path: '', component: MainPageComponent },
   { path: '404', component: Page404Component },
   { path: '**', redirectTo: '/404' },
