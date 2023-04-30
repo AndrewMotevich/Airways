2bb84023619c8594cd6ecc0945a4b3cc6e6821fc
Anna Iustus <ajustusa@gmail.com>
2023-04-30 17:54:21 +0200

    feat: add ticket-date-slider

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketDateSliderModule } from './shared/ticket-date-slider/ticket-date-slider.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TicketDateSliderModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
