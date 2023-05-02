85887a73cb63290c009591dd9718b5f1a68acd44
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-02 19:45:31 +0300

    feat: add draft main page, header, footer

--- airways/src/app/core/modules/core/core.module.ts
+++ airways/src/app/core/modules/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
 import { HeaderComponent } from '../../components/header/header/header.component';
 import { FooterComponent } from '../../components/header/footer/footer.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSelectModule, MatButtonModule, MatIconModule, MatStepperModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
