1b8d05387f5d0fab196b50074e04d86eb1f0132f
Anna Iustus <ajustusa@gmail.com>
2023-05-13 16:40:50 +0200

    feat: combined layouts for flight selection page

--- airways/src/app/booking/pages/select-flight/select-flight.component.ts
+++ airways/src/app/booking/pages/select-flight/select-flight.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IFlightDetails } from '../../models/flight-details.interface';
+import { FlightsDataService } from '../../services/flightsData.service';
 
 @Component({
   selector: 'app-select-flight',
   templateUrl: './select-flight.component.html',
-  styleUrls: ['./select-flight.component.scss']
+  styleUrls: ['./select-flight.component.scss'],
+  providers: [FlightsDataService],
 })
-export class SelectFlightComponent {
+export class SelectFlightComponent implements OnInit {
+  flightsDetails$!: Observable<IFlightDetails[]>;
 
+  constructor(private flightsDataService: FlightsDataService) {}
+
+  ngOnInit(): void {
+    this.flightsDetails$ = this.flightsDataService.getFlightsData();
+  }
 }
