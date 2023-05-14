010e87c29989e272017c5dd487c8202e3655c3cd
Anna Iustus <ajustusa@gmail.com>
2023-05-14 18:07:55 +0200

    WIP: ticketsData for ticket-date-slider

--- airways/src/app/booking/pages/select-flight/select-flight.component.ts
+++ airways/src/app/booking/pages/select-flight/select-flight.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component, OnInit, Output } from '@angular/core';
+import { Observable, combineLatest, map, of, tap } from 'rxjs';
+import dayjs from 'dayjs';
 import { IFlightDetails } from '../../models/flight-details.interface';
 import { FlightsDataService } from '../../services/flightsData.service';
 
@@ -16,21 +18,48 @@ export class SelectFlightComponent implements OnInit {
 
   arrival: string = 'Warsaw Modlin';
 
+  ticketsData: { date: string; cost: string }[] = [];
+
+  @Output() departureDate: string = '2023-05-15';
+
+  @Output() returnDate: string = '2023-05-17';
+
   constructor(private flightsDataService: FlightsDataService) {}
 
   ngOnInit(): void {
     this.flightsDetails$ = this.flightsDataService
-      .getFlightsData(
-        'LON',
-        'LED',
-        '2023-05-16',
-        '2023-05-25',
-        'eur',
-        true
-      )
-      .pipe(map((flightsData) => flightsData));
+      .getFlightsData('BEG', 'LED', this.departureDate, this.returnDate, 'eur', false)
+      .pipe(
+        tap((flightsData) => {
+          this.ticketsData = flightsData.map((flight) => ({
+            date: dayjs(flight.departure_at).format('YYYY-MM-DD'),
+            cost: flight.price.toString(),
+          }));
+        })
+      );
   }
-
 }
+// ngOnInit(): void {
+//   const flightsData$ = this.flightsDataService
+//     .getFlightsData('BEG', 'LED', this.departureDate, this.returnDate, 'eur', false)
+//     .pipe(
+//       tap((flightsData) => {
+//         this.ticketsData = flightsData.map((flight) => ({
+//           date: dayjs(flight.departure_at).format('YYYY-MM-DD'),
+//           cost: flight.price.toString(),
+//         }));
+//       })
+//     );
 
-
+//   this.flightsDetails$ = combineLatest([flightsData$, of(this.ticketsData)]).pipe(
+//     map(([flightsData, ticketsData]) => flightsData.map((flight) => ({
+//         ...flight,
+//         date: dayjs(flight.departure_at).format('YYYY-MM-DD'),
+//         cost: flight.price.toString(),
+//         ticketData: ticketsData.find(
+//           (t) => t.date === dayjs(flight.departure_at).format('YYYY-MM-DD')
+//         )?.cost,
+//       })))
+//   );
+// }
+// }
