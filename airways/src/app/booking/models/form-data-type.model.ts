17866a2f8cfbf03dda47ff96228c1f168e6a47e8
Anna Iustus <ajustusa@gmail.com>
2023-05-20 16:25:06 +0200

    fix: add code and city to formDataService

--- airways/src/app/booking/models/form-data-type.model.ts
+++ airways/src/app/booking/models/form-data-type.model.ts
@@ -1,11 +1,16 @@
-export type FormDataType = {
+export interface FormDataModel<T> {
   roundedTrip: string | null;
-  from: string | null;
-  destination: string | null;
+  from: T | null;
+  destination: T | null;
   dateStart: Date | null;
   dateEnd: Date | null;
   passengers: number | null;
   adult: number | null;
   child: number | null;
   infant: number | null;
-};
+}
+
+export interface PointModel {
+  title: string | null;
+  code: string | null;
+}
--- airways/src/app/booking/services/form-data.service.ts
+++ airways/src/app/booking/services/form-data.service.ts
@@ -1,21 +1,49 @@
+/* eslint-disable class-methods-use-this */
 import { Injectable } from '@angular/core';
-import { FormDataType } from '../models/form-data-type.model';
+import { FormDataModel, PointModel } from '../models/form-data-type.model';
+
+const initialFormDataValues: FormDataModel<PointModel> = {
+  roundedTrip: null,
+  from: null,
+  destination: null,
+  dateStart: null,
+  dateEnd: null,
+  passengers: null,
+  adult: null,
+  child: null,
+  infant: null,
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormDataService {
-  private formData?: FormDataType;
+  private formData: FormDataModel<PointModel> = initialFormDataValues;
 
-  setMainFormData(mainFormData: FormDataType): void {
-    this.formData = mainFormData;
-    this.formData.from = this.formData.from?.match(/\(\w+\)/)?.[0] || '';
-    this.formData.destination = this.formData.destination?.match(/\(\w+\)/)?.[0] || '';
-    this.formData.from = this.formData.from?.match(/\w+/)?.[0] || '';
-    this.formData.destination = this.formData.destination?.match(/\w+/)?.[0] || '';
+  setMainFormData(mainFormData: FormDataModel<string>): void {
+    this.formData = {
+      ...mainFormData,
+      from: this.getCityAndCodeFromString(mainFormData.from),
+      destination: this.getCityAndCodeFromString(mainFormData.destination),
+    };
   }
 
-  getMainFormData(): FormDataType | undefined {
+  getMainFormData(): FormDataModel<PointModel> {
     return this.formData;
   }
+
+  private getCityAndCodeFromString = (combinedString: string | null): PointModel => {
+    if (combinedString === null)
+      return {
+        title: null,
+        code: null,
+      };
+    const matchCity = combinedString.match(/^(.*?)\s*\([^)]*\)/);
+    const matchCode = combinedString.match(/\((.*?)\)/);
+
+    return {
+      title: matchCity ? matchCity[1] : null,
+      code: matchCode ? matchCode[1] : null,
+    };
+  };
 }
