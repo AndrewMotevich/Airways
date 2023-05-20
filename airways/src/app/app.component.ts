3e4acf410d07d673d3dcd6e4d1e1f649ff137957
azozulya <bbbom@mail.ru>
2023-05-20 21:41:44 +0300

    refactor: add adaptive for main page

--- airways/src/app/app.component.ts
+++ airways/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  @HostBinding('class.main-page') isMain: boolean = true;
+
+  @HostBinding('class') theme: 'light-theme' | 'dark-theme' = 'light-theme';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isMain = event.url === '/';
+      }
+    });
+  }
+}
