394906b549c23697b31aead6b7f95c56e1b07753
Anna Justus <50149163+Iogsotot@users.noreply.github.com>
2023-05-23 23:47:24 +0300

    feat: data for slider for flight selection page#6 (#36)
    * feat: add dayjs and schematics in deps
    
    * feat: add ticket-date-slider
    
    * feat: add real data for slider
    
    * feat: add real data for slider
    
    * fix: add code and city to formDataService
    
    * feat: add prev-next date logic in slider
    
    * refactor: do not send repeated requests if the data has not changed
    
    * feat: disable prev btn in slider if the date is earlier than the current one
    
    * fix: correct styles for current, future, past cards in slider

--- airways/src/app/booking/services/form-data.service.ts
+++ airways/src/app/booking/services/form-data.service.ts
@@ -1,21 +1,69 @@
+/* eslint-disable class-methods-use-this */
 import { Injectable } from '@angular/core';
-import { FormDataType } from '../models/form-data-type.model';
+import { FlightDirection, FormDataModel, PointModel } from '../models/form-data.model';
+
+const initialFormDataValues: FormDataModel<PointModel> = {
+  roundedTrip: null,
+  from: null,
+  destination: null,
+  dateStart: null,
+  dateEnd: null,
+  passengers: null,
+  adult: null,
+  child: null,
+  infant: null,
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormDataService {
-  private formData?: FormDataType;
+  private flightData: FormDataModel<PointModel> = initialFormDataValues;
 
-  setMainFormData(mainFormData: FormDataType): void {
-    this.formData = mainFormData;
-    this.formData.from = this.formData.from?.match(/\(\w+\)/)?.[0] || '';
-    this.formData.destination = this.formData.destination?.match(/\(\w+\)/)?.[0] || '';
-    this.formData.from = this.formData.from?.match(/\w+/)?.[0] || '';
-    this.formData.destination = this.formData.destination?.match(/\w+/)?.[0] || '';
+  private getCityAndCodeFromString = (combinedString: string | null): PointModel => {
+    if (combinedString === null)
+      return {
+        title: null,
+        code: null,
+      };
+    const matchCity = combinedString.match(/^(.*?)\s*\([^)]*\)/);
+    const matchCode = combinedString.match(/\((.*?)\)/);
+
+    return {
+      title: matchCity ? matchCity[1] : null,
+      code: matchCode ? matchCode[1] : null,
+    };
+  };
+
+  setMainFormData(mainFormData: FormDataModel<string>): void {
+    this.flightData = {
+      ...mainFormData,
+      from: this.getCityAndCodeFromString(mainFormData.from),
+      destination: this.getCityAndCodeFromString(mainFormData.destination),
+    };
   }
 
-  getMainFormData(): FormDataType | undefined {
-    return this.formData;
+  getMainFormData(): FormDataModel<PointModel> {
+    return this.flightData;
+  }
+
+  setFlightDataDate(date: Date, direction: FlightDirection): void {
+    const currentFlightDataState: FormDataModel<PointModel> = this.flightData;
+    const newFlightDataState: FormDataModel<PointModel> = { ...currentFlightDataState };
+    if (direction === FlightDirection.DEPARTURE) {
+      newFlightDataState.dateStart = date;
+      newFlightDataState.dateEnd =
+        currentFlightDataState.dateEnd !== null && date >= currentFlightDataState.dateEnd
+          ? date
+          : currentFlightDataState.dateEnd;
+    }
+    if (direction === FlightDirection.ARRIVAL) {
+      newFlightDataState.dateEnd = date;
+      newFlightDataState.dateStart =
+        currentFlightDataState.dateStart !== null && date <= currentFlightDataState.dateStart
+          ? date
+          : currentFlightDataState.dateStart;
+    }
+    this.flightData = newFlightDataState;
   }
 }
--- airways/src/app/shared/ticket-date-slider/ticket-date-slider.component.ts
+++ airways/src/app/shared/ticket-date-slider/ticket-date-slider.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 import dayjs from 'dayjs';
 
+export const enum CurrentDateStateEnum {
+  PAST = 'past',
+  CURRENT = 'current',
+  FUTURE = 'future',
+}
 @Component({
   selector: 'app-ticket-date-slider',
   templateUrl: './ticket-date-slider.component.html',
   styleUrls: ['./ticket-date-slider.component.scss'],
 })
-export class TicketDateSliderComponent {
-  @Input() selectedDate: string = '';
+export class TicketDateSliderComponent implements OnChanges {
+  @Input() selectedDate!: string;
 
   @Input() set ticketsData(value: { date: string; cost: string }[] | null) {
     const dates = [
@@ -21,9 +26,50 @@ export class TicketDateSliderComponent {
       const ticket = value?.find((t) => t.date === date.format('YYYY-MM-DD'));
       return { date: date.format('DD MMM dddd'), cost: ticket ? ticket.cost : null };
     });
+
+    this.dateStates = dates.map((date) => {
+      if (
+        dayjs().isBefore(dayjs(date).toDate(), 'day') ||
+        dayjs().isSame(dayjs(date, 'day').toDate())
+      ) {
+        return CurrentDateStateEnum.FUTURE;
+      }
+
+      if (
+        dayjs().isAfter(dayjs(date).toDate(), 'day') &&
+        !dayjs().isSame(dayjs(date, 'day').toDate())
+      ) {
+        return CurrentDateStateEnum.PAST;
+      }
+      return CurrentDateStateEnum.FUTURE;
+    });
   }
 
+  @Output() nextDateButtonClick: EventEmitter<void> = new EventEmitter<void>();
+
+  @Output() prevDateButtonClick: EventEmitter<void> = new EventEmitter<void>();
+
+  onNextDateButtonClick(): void {
+    this.nextDateButtonClick.emit();
+  }
+
+  isDisablePrevBtn: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selectedDate'] && this.selectedDate) {
+      this.isDisablePrevBtn = !dayjs().isBefore(dayjs(this.selectedDate).toDate());
+    }
+  }
+
+  onPrevDateButtonClick(): void {
+    if (!this.isDisablePrevBtn) {
+      this.prevDateButtonClick.emit();
+    }
+  }
+  
   boundColor: string = 'rgb(255, 153, 0)';
 
   selectedDates: { date: string; cost: string | null }[] = [];
+
+  dateStates: CurrentDateStateEnum[] = [];
 }
