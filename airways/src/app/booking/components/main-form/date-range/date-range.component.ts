1aad9f2554a01dca3b5cad2777413ff1267ed7b0
azozulya <bbbom@mail.ru>
2023-05-26 11:01:16 +0300

    fix: add error message, change subscription logic

--- airways/src/app/booking/components/main-form/date-range/date-range.component.ts
+++ airways/src/app/booking/components/main-form/date-range/date-range.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormGroupDirective } from '@angular/forms';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
 import dayjs from 'dayjs';
-import { HeaderDataService } from 'src/app/core/services/header-data.service';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { HeaderDataService } from '../../../../core/services/header-data.service';
 import { CustomFormat } from '../../../../shared/utils/date-format';
 
 @Component({
@@ -13,24 +14,30 @@ import { CustomFormat } from '../../../../shared/utils/date-format';
     { provide: MAT_DATE_FORMATS, useClass: CustomFormat },
   ],
 })
-export class DateRangeComponent implements OnInit {
+export class DateRangeComponent implements OnInit, OnDestroy {
+  private subcription!: Subscription | null;
+
   formGroup!: FormGroup;
 
-  dateFormat!: string;
+  dateFormat$!: BehaviorSubject<string>;
 
   minDate = dayjs().toDate();
 
   maxDate = dayjs().add(1, 'y').toDate();
+
+  get dateEnd(): FormControl<string> {
+    return this.formGroup.get('dateEnd') as FormControl;
+  }
 
   constructor(private fg: FormGroupDirective, private headerDateService: HeaderDataService, @Inject(MAT_DATE_FORMATS) public config: any) { }
 
   ngOnInit(): void {
     this.formGroup = this.fg.control;
 
-    this.headerDateService.currentDateFormat.subscribe((newDateFormat: string) => {
-      this.dateFormat = newDateFormat;
-      this.config.currentDateFormat = newDateFormat;
+    this.dateFormat$ = this.headerDateService.currentDateFormat$;
 
+    this.subcription = this.dateFormat$.subscribe((newDateFormat: string) => {
+      this.config.currentDateFormat = newDateFormat;
       this.formatDates();
     });
   }
@@ -42,5 +49,12 @@ export class DateRangeComponent implements OnInit {
     this.formGroup.patchValue({
       dateStart, dateEnd
     })
+  }
+
+  ngOnDestroy(): void {
+    if (this.subcription) {
+      this.subcription.unsubscribe();
+      this.subcription = null;
+    }
   }
 }
