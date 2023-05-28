602bc48c5372d3b3f24e8d9fd99a2fe38692f709
Anna Iustus <ajustusa@gmail.com>
2023-05-28 21:56:20 +0200

    fix: oneWay logic

--- airways/src/app/booking/pages/select-flight/select-flight.component.ts
+++ airways/src/app/booking/pages/select-flight/select-flight.component.ts
@@ -81,7 +81,6 @@ export class SelectFlightComponent implements OnInit {
     private flightsDataService: FlightsDataService,
     private formDataService: FormDataService,
     private headerDataService: HeaderDataService,
-    // private tripData: TripDataService,
     private ticketsDataService: TicketsDataService,
     private router: Router
   ) {
@@ -136,6 +135,7 @@ export class SelectFlightComponent implements OnInit {
     });
 
     this.ticketsData$.subscribe((tickets: IFlightDetails[]) => {
+      console.log(tickets);
       this.departureTicketsCount = tickets.filter(
         (ticket) => ticket.destination_airport === this.arrival.code
       ).length;
@@ -145,8 +145,14 @@ export class SelectFlightComponent implements OnInit {
       ).length;
 
       this.allDepartureTicketsSelected = this.departureTicketsCount >= 1;
-      this.allReturnTicketsSelected = this.returnTicketsCount >= 1;
+      this.allReturnTicketsSelected = this.isOneWay ? this.isOneWay : this.returnTicketsCount >= 1;
       this.allTicketsSelected = this.allDepartureTicketsSelected && this.allReturnTicketsSelected;
+      console.log(
+        this.allTicketsSelected,
+        this.allDepartureTicketsSelected,
+        this.allReturnTicketsSelected,
+        this.isOneWay
+      );
     });
   }
 
